8a5d0c3046b8291f19fe3f00e05cae50613daf2d
Karthic Ashokan <karthic@drivemotors.com>
2022-05-30 20:18:23 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="App">
+            <div className="app-content">
+                <h1>test</h1>
+            </div>
+        </div>
+    );
+}
+
+export default App;
